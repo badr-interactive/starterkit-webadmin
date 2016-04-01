ae80bc6bfaa4fa6cad64fa791c4aaad0708a6832
Salman El Farisi <dev.salman.farisi@gmail.com>
2016-04-01 16:13:45 +0700

    change activation_token column definition

---
+++ database/migrations/2016_04_01_113913_modify_activation_token_column.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyActivationTokenColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('activation_token')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('activation_token', 8)->change();
+        });
+    }
+}
