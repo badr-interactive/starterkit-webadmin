58b5abd03763b985775dea3be616d4786330a6be
Salman El Farisi <dev.salman.farisi@gmail.com>
2016-04-01 17:09:25 +0700

    add is_superrole in user_roles table

--- database/migrations/2016_04_01_170723_add_is_admin_column_in_user_roles.php
+++ database/migrations/2016_04_01_170723_add_is_admin_column_in_user_roles.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddIsSuperuserColumnInUsers extends Migration
+class AddIsAdminColumnInUserRoles extends Migration
 {
     /**
      * Run the migrations.
@@ -12,8 +12,8 @@ class AddIsSuperuserColumnInUsers extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_superuser')->default(0)->after('is_active');
+        Schema::table('user_roles', function (Blueprint $table) {
+            $table->boolean('is_admin')->default(0)->after('description');
         });
     }
 
@@ -24,8 +24,8 @@ class AddIsSuperuserColumnInUsers extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('is_superuser');
+        Schema::table('user_roles', function (Blueprint $table) {
+            $table->dropColumn('is_admin');
         });
     }
 }
