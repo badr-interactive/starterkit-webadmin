80403c284af6f91f2b40cc702c445095594801c4
Salman El Farisi <dev.salman.farisi@gmail.com>
2016-04-01 17:02:53 +0700

    add is_superuser column

---
+++ database/migrations/2016_04_01_170111_add_is_superuser_column_in_users.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsSuperuserColumnInUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('is_superuser')->default(0)->after('is_active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('is_superuser');
+        });
+    }
+}
