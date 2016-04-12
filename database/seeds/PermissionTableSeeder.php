2de8b9457b89b5d14ee4c9b7164ffd4675faa23c
Salman El Farisi <dev.salman.farisi@gmail.com>
2016-04-12 15:20:14 +0700

    fix permission table seeder

--- database/seeds/PermissionTableSeeder.php
+++ database/seeds/PermissionTableSeeder.php
@@ -16,6 +16,7 @@ class PermissionTableSeeder extends Seeder
                 'uuid' => 'f09e8d05-312b-4ba9-8fff-5452ceb0d119',
                 'name' => 'manage-users',
                 'display_name' => 'Manage User',
+                'description' => 'Create, Update and Delete User Account',
                 'created_at' => '2016-02-01 10:00:00',
                 'updated_at' => '2016-02-01 10:00:00'
             ],
@@ -23,6 +24,7 @@ class PermissionTableSeeder extends Seeder
                 'uuid' => '9e1ced4f-07ec-4c68-8b3a-149d55df5bd2',
                 'name' => 'manage-roles',
                 'display_name' => 'Manage Roles',
+                'description' => 'Create, Update and Delete User Role',
                 'created_at' => '2016-02-01 10:00:00',
                 'updated_at' => '2016-02-01 10:00:00'
             ],
@@ -30,6 +32,7 @@ class PermissionTableSeeder extends Seeder
                 'uuid' => '83378eeb-f4f9-48bb-a4fd-5c2d4c110347',
                 'name' => 'manage-controls',
                 'display_name' => 'Manage Controls',
+                'description' => 'Create, Update and Delete User Permission',
                 'created_at' => '2016-02-01 10:00:00',
                 'updated_at' => '2016-02-01 10:00:00'
             ]
