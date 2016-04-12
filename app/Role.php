14a98d7ef0f4f1c9b623f33b21d1763ec8b0c74b
Salman El Farisi <dev.salman.farisi@gmail.com>
2016-04-12 15:25:45 +0700

    fix created_at and updated_at format

--- app/Role.php
+++ app/Role.php
@@ -3,9 +3,20 @@
 namespace App;
 
 use Zizaco\Entrust\EntrustRole;
+use Carbon\Carbon;
 
 class Role extends EntrustRole
 {
     protected $table = 'roles';
     protected $fillable = ['uuid', 'name', 'display_name', 'description', 'created_at', 'updated_at'];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return date('d M Y', strtotime($value));
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s', $value)->diffForHumans();
+    }
 }
