a9b583d5b75ebc167715e43b5defa1a2adc2288d
Salman El Farisi <dev.salman.farisi@gmail.com>
2016-04-19 11:20:43 +0700

    refactoring user registration & update process

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -56,29 +56,18 @@ class UserController extends Controller
         $user->email = $request->get('email');
         $user->name = $request->get('name');
         $user->phone = $request->get('phone');
-
-        if ($this->isNewUserRequest($request)) {
-            $generatedPassword = bin2hex(random_bytes(8));
-            $user->password = bcrypt($generatedPassword);
-            $user->activation_token = bin2hex(random_bytes(8));
-            $user->uuid = \Uuid::generate(4);
-
-            $image = $this->identicon->getImageData($request->name);
-            Storage::put('avatars/' . $user->uuid . '.png', $image);
-            Event::fire(new UserHasRegistered($user, $generatedPassword));
-        }
-
         $user->save();
-        $role = $this->role->where(['uuid' => $request->role_id])->first();
-        $user->attachRole($role);
 
-        if (empty($uuid)) {
-            $request->session()->flash('alert-success', 'User was successfully saved! '
-                . 'Please inform the user to check their email for account activation');
+        $role = $this->role->where(['uuid' => $request->role_id])->first();
+        if ($this->isNewUser($user)) {
+            $this->registerNewUser($user, $role);
+            $message = trans('users.registered', ['name' => $user->name]);
         } else {
-            $request->session()->flash('alert-success', 'User account was successfully updated!');
+            $this->updateUserRole($user, $role);
+            $message = trans('users.updated', ['name' => $user->name]);
         }
 
+        $request->session()->flash('alert-success', $message);
         return redirect()->back();
     }
 
@@ -126,8 +115,28 @@ class UserController extends Controller
         abort(404);
     }
 
-    private function isNewUserRequest(Request $request)
+    private function updateUserRole(User $user, Role $role)
     {
-        return empty($request->uuid);
+        $user->detachRoles();
+        $user->attachRole($role);
+    }
+
+    private function isNewUser(User $user)
+    {
+        return $user->uuid === '0';
+    }
+
+    private function registerNewUser(User $user, Role $role)
+    {
+        $generatedPassword = bin2hex(random_bytes(8));
+        $user->password = bcrypt($generatedPassword);
+        $user->activation_token = bin2hex(random_bytes(8));
+        $user->uuid = \Uuid::generate(4);
+        $user->save();
+        $user->attachRole($role);
+
+        $image = $this->identicon->getImageData($user->name);
+        Storage::put('avatars/' . $user->uuid . '.png', $image);
+        Event::fire(new UserHasRegistered($user, $generatedPassword));
     }
 }
---
+++ resources/lang/en/users.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'registered' => 'User account ":name" was successfully saved! Please inform the user to check their email for account activation',
+    'updated' => 'User account ":name" was successfully updated!',
+];
