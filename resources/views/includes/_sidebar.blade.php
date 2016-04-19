de3bb15e87af6e4596a27a7157e3b026fa422c52
Salman El Farisi <dev.salman.farisi@gmail.com>
2016-04-19 11:52:09 +0700

    fix typo

--- resources/views/includes/_sidebar.blade.php
+++ resources/views/includes/_sidebar.blade.php
@@ -4,15 +4,15 @@
     <section class="sidebar">
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu">
-            @permission('manages-users')
+            @permission('manage-users')
                 <li><a href="{{route('user.index')}}"><i class="ion ion-person"></i> Users</a></li>
             @endpermission
 
-            @permission('manages-roles')
+            @permission('manage-roles')
             <li><a href="{{route('user_role.index')}}"><i class="ion ion-locked"></i> User Roles</a></li>
             @endpermission
-            
-            @permission('manages-controls')
+
+            @permission('manage-controls')
             <li><a href="{{route('control.index')}}"><i class="ion ion-locked"></i> Permissions</a></li>
             @endpermission
         </ul>
