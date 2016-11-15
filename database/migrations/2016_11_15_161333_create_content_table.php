79e05de88d1dd49172d805f25cd44cef45ccd8ef
Salman El Farisi <salman.farisi@badr-interactive.com>
2016-11-15 16:29:00 +0700

    add migrations for contents table

---
+++ database/migrations/2016_11_15_161333_create_content_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uuid', 36);
+            $table->string('title', 50);
+            $table->string('image', 50);
+            $table->text('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('contents');
+    }
+}
